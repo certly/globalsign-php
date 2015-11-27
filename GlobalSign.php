d5406f5b0afe6b3232bf8590cdabd41d62b629a0
Ian Carroll <ian@ian.sh>
2015-11-26 23:13:50 -0500

    Initial scaffolding.

---
+++ GlobalSign.php
@@ -0,0 +1,49 @@
+<?php
+namespace Certly\GlobalSign;
+
+class GlobalSign
+{
+    /**
+    * The SoapClient instance used to make requests.
+    *
+    * @var SoapClient
+    */
+    protected $soap;
+
+    /**
+    * The GlobalSign account username.
+    *
+    * @var string
+    */
+    protected $username;
+
+    /**
+    * The GlobalSign account password.
+    *
+    * @var string
+    */
+    protected $password;
+
+    /**
+    * The indicator of whether or not to use GlobalSign's production API.
+    *
+    * @var string
+    */
+    protected $production;
+
+    /**
+     * Create a new GlobalSign API wrapper.
+     *
+     * @param  string  $username
+     * @param  string  $password
+     * @param  boolean $production
+     * @return void
+     */
+    public function __construct($username, $password, $production = true)
+    {
+        $this->soap = new SoapClient;
+        $this->username = $username;
+        $this->password = $password;
+        $this->production = $production;
+    }
+}

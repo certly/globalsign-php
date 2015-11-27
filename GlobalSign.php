e82eae6016e09ac3651016796c86fa77a6813cf9
Ian Carroll <iangcarroll+spam-only@icloud.com>
2015-11-26 23:17:33 -0500

    Applied fixes from StyleCI

--- GlobalSign.php
+++ GlobalSign.php
@@ -1,47 +1,49 @@
 <?php
+
 namespace Certly\GlobalSign;
 
 class GlobalSign
 {
     /**
-    * The SoapClient instance used to make requests.
-    *
-    * @var SoapClient
-    */
+     * The SoapClient instance used to make requests.
+     *
+     * @var SoapClient
+     */
     protected $soap;
 
     /**
-    * The GlobalSign account username.
-    *
-    * @var string
-    */
+     * The GlobalSign account username.
+     *
+     * @var string
+     */
     protected $username;
 
     /**
-    * The GlobalSign account password.
-    *
-    * @var string
-    */
+     * The GlobalSign account password.
+     *
+     * @var string
+     */
     protected $password;
 
     /**
-    * The indicator of whether or not to use GlobalSign's production API.
-    *
-    * @var string
-    */
+     * The indicator of whether or not to use GlobalSign's production API.
+     *
+     * @var string
+     */
     protected $production;
 
     /**
      * Create a new GlobalSign API wrapper.
      *
-     * @param  string  $username
-     * @param  string  $password
-     * @param  boolean $production
+     * @param string $username
+     * @param string $password
+     * @param bool   $production
+     *
      * @return void
      */
     public function __construct($username, $password, $production = true)
     {
-        $this->soap = new SoapClient;
+        $this->soap = new SoapClient();
         $this->username = $username;
         $this->password = $password;
         $this->production = $production;
